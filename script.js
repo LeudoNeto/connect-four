4b547913203642b7e7b91fd2b3968e4e8a6ec229
Leudo Neto <leudoneto1234@gmail.com>
2022-07-27 20:25:18 -0300

    final

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let game = [['','','','','','',''],
             ['','','','','','',''],
             ['','','','','','','']];
 
+let win_message = document.querySelector('#win-message')
+
 function check_game()
 {
     for (line of game)
@@ -13,7 +15,7 @@ function check_game()
         {
             if (line[i] && line[i] === line[i+1] && line[i+1] === line[i+2] && line[i+2] === line[i+3])
             {
-                return true;
+                return line[i];
             }
         }
     }
@@ -23,18 +25,19 @@ function check_game()
         {
             if (game[o][i] && game[o][i] === game[o+1][i] && game[o+1][i] === game[o+2][i] && game[o+2][i] === game[o+3][i])
             {
-                return true;
+                return game[o][i];
             }
             if (game[o][i] && game[o][i] === game[o+1][i+1] && game[o+1][i+1] === game[o+2][i+2] && game[o+2][i+2] === game[o+3][i+3])
             {
-                return true;
+                return game[o][i];
             }
             if (game[o][i] && game[o][i] === game[o+1][i-1] && game[o+1][i-1] === game[o+2][i-2] && game[o+2][i-2] === game[o+3][i-3])
             {
-                return true;
+                return game[o][i];
             }
         }
     }
+    return false;
 }
 
 circles = Array.from(document.querySelectorAll('td button'));
@@ -48,34 +51,56 @@ for (circle of circles)
     circle.style.background = 'gray';
     circle.addEventListener('click',function()
     {
-        if (game[0][this.column] === '')
+        if (game[0][this.column] === '' && !check_game())
         {
-            o = 0;
-            if (circles[7*o + this.column].style.background == 'gray')
+            for (let o = 5; o >= 0; o--)
             {
-                if (clicks % 2 === 0)
+                if (circles[7*o + this.column].style.background == 'gray')
                 {
-                    circles[7*o + this.column].style.background = 'blue';
-                }
-                else
-                {
-                    circles[7*o + this.column].style.background = 'red';
+                    if (clicks % 2 === 0)
+                    {
+                        circles[7*o + this.column].style.background = 'blue';
+                        game[o][this.column] ='Blue';
+                    }
+                    else
+                    {
+                        circles[7*o + this.column].style.background = 'red';
+                        game[o][this.column] ='Red';
+                    }
+                    break;
                 }
             }
-            fall = setInterval(() => {
-                if (o >= 4)
-                {
-                    clearInterval(fall)
-                }
-                if (circles[7*(o+1) + this.column].style.background == 'gray')
-                {
-                    circles[7*o + this.column].style.background = 'gray'
-                    if (clicks % 2 === 0) {circles[7*(o+1) + this.column].style.background = 'red'}
-                    else {circles[7*(o+1) + this.column].style.background = 'blue'}
-                }
-                o++;
-            }, 150);
         clicks++;
+        }
+        if (check_game())
+        {
+            win_message.children[0].textContent = `${check_game()} won.`;
+            win_message.style.display = 'block';
+            window.scrollTo(0,document.body.scrollHeight);
         }
     })
 }
+
+restart = document.querySelector('#restart-button');
+
+restart.addEventListener('click',function()
+{
+    clicks = 0;
+    game = [['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','','']];
+
+    for (circle of circles)
+    {
+        circle.style.background = 'gray';
+    }
+    if (win_message.children[0].textContent)
+    {
+        win_message.children[0].textContent = '';
+        win_message.style.display = 'none';
+        window.scrollTo(0,0);
+    }
+})
